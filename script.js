97991599d4bfde1a58696dbd6bcce39aeb1657af
vhanvergara0628-glitch <vhanvergara0628@gmail.com>
2026-04-14 11:35:37 +0800

    added some bg music hehehe

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const button_icons = document.querySelectorAll(".icon");
 const audio = new Audio("./sfx/click.mp3");
+const background_music = new Audio("./sfx/background.mp3")
 
+background_music.play();
+background_music.volume = 0.1;
+background_music.loop = true;
 
 button_icons.forEach(function(click){
 click.addEventListener("click", function(){
