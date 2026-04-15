08e6f5b881f8ead0b85b5fb9ec0dd4eb80b6b5de
vhanvergara0628-glitch <vhanvergara0628@gmail.com>
2026-04-15 14:08:45 +0800

    modal update, can open and can close aswell

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
+// Elements
 const button_icons = document.querySelectorAll(".icon");
-const audio = new Audio("./sfx/click.mp3");
-const background_music = new Audio("./sfx/background.mp3")
 const music_switch = document.querySelector("#music_switch");
 
+const modal_one = document.querySelector(".modal_one");
+const one_modal = document.querySelector("#one_modal");
+const close_modal = document.querySelector("#close_modal");
 
+// Audio
+const audio = new Audio("./sfx/click.mp3");
+const background_music = new Audio("./sfx/background.mp3");
+
+// Background music settings
 background_music.volume = 0.070;
 background_music.loop = true;
 
-button_icons.forEach(function(click){
-click.addEventListener("click", function(){
+// Modal open
+one_modal.addEventListener("click", function () {
+  modal_one.classList.add("active");
+});
+
+// Modal close
+close_modal.addEventListener("click", function () {
+  modal_one.classList.remove("active");
+});
+
+// Button click sound
+button_icons.forEach(function (click) {
+  click.addEventListener("click", function () {
     audio.play();
-});
+  });
 });
 
-music_switch.addEventListener("click", function(){
-    
- audio.play();
- if(background_music.paused){
-   background_music.play();
- } else{
+// Music toggle
+music_switch.addEventListener("click", function () {
+  audio.play();
+
+  if (background_music.paused) {
+    background_music.play();
+  } else {
     background_music.pause();
- }
+  }
 });
-
-
