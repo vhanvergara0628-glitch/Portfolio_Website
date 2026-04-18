8d5346af82ceb1ce19976b27c615a57e9b8177a6
vhanvergara0628-glitch <vhanvergara0628@gmail.com>
2026-04-18 17:50:25 +0800

    i made the other icons functional, and pinalinis and pina organized natin ang magulong code ko sa ai. (not vibe coding) pinalinis lang!

--- script.js
+++ script.js
@@ -3,16 +3,28 @@ const button_icons = document.querySelectorAll(".icon");
 const music_switch = document.querySelector("#music_switch");
 const all_buttons = document.querySelectorAll("button");
 
-//icon modals
+// Icon modals
 const modal_one = document.querySelector(".modal_one");
 const modal_two = document.querySelector(".modal_two");
+const modal_three = document.querySelector(".modal_three");
+const modal_four = document.querySelector(".modal_four");
+const modal_five = document.querySelector(".modal_five");
+const modal_six = document.querySelector(".modal_six");
 
-
-
-//open and close
+// Open and close buttons
 const one_modal = document.querySelector("#one_modal");
 const button_aboutme = document.querySelector("#button_aboutme");
+const button_projects = document.querySelector("#button_projects");
+const button_skills = document.querySelector("#button_skills");
+const button_creatives = document.querySelector("#button_creatives");
+const button_stuff = document.querySelector("#button_stuff");
+
 const close_modal = document.querySelector("#close_modal");
+const close_modal2 = document.querySelector("#close_modal2");
+const close_modal3 = document.querySelector("#close_modal3");
+const close_modal4 = document.querySelector("#close_modal4");
+const close_modal5 = document.querySelector("#close_modal5");
+const close_modal6 = document.querySelector("#close_modal6");
 
 // Audio
 const audio = new Audio("./sfx/click.mp3");
@@ -22,23 +34,54 @@ const background_music = new Audio("./sfx/background.mp3");
 background_music.volume = 0.070;
 background_music.loop = true;
 
-// Modal openerssss!!
+// Modal openers
 one_modal.addEventListener("click", function () {
   modal_one.classList.add("active");
 });
 
-button_aboutme.addEventListener("click", function(){
-    modal_two.classList.add("active");
+button_aboutme.addEventListener("click", function () {
+  modal_two.classList.add("active");
 });
 
+button_projects.addEventListener("click", function () {
+  modal_three.classList.add("active");
+});
 
+button_skills.addEventListener("click", function () {
+  modal_four.classList.add("active");
+});
 
+button_creatives.addEventListener("click", function () {
+  modal_five.classList.add("active");
+});
 
+button_stuff.addEventListener("click", function () {
+  modal_six.classList.add("active");
+});
 
-// Modal close
+// Modal closers
 close_modal.addEventListener("click", function () {
   modal_one.classList.remove("active");
- 
+});
+
+close_modal2.addEventListener("click", function () {
+  modal_two.classList.remove("active");
+});
+
+close_modal3.addEventListener("click", function () {
+  modal_three.classList.remove("active");
+});
+
+close_modal4.addEventListener("click", function () {
+  modal_four.classList.remove("active");
+});
+
+close_modal5.addEventListener("click", function () {
+  modal_five.classList.remove("active");
+});
+
+close_modal6.addEventListener("click", function () {
+  modal_six.classList.remove("active");
 });
 
 // Button click sound
