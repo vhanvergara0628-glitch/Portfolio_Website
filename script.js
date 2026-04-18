946fad80250856b42016b7fa742b09d7331edd32
vhanvergara0628-glitch <vhanvergara0628@gmail.com>
2026-04-18 09:36:27 +0800

    lil update

--- script.js
+++ script.js
@@ -2,8 +2,16 @@
 const button_icons = document.querySelectorAll(".icon");
 const music_switch = document.querySelector("#music_switch");
 const all_buttons = document.querySelectorAll("button");
+
+//icon modals
 const modal_one = document.querySelector(".modal_one");
+const modal_two = document.querySelector(".modal_two");
+
+
+
+//open and close
 const one_modal = document.querySelector("#one_modal");
+const button_aboutme = document.querySelector("#button_aboutme");
 const close_modal = document.querySelector("#close_modal");
 
 // Audio
@@ -14,14 +22,23 @@ const background_music = new Audio("./sfx/background.mp3");
 background_music.volume = 0.070;
 background_music.loop = true;
 
-// Modal open
+// Modal openerssss!!
 one_modal.addEventListener("click", function () {
   modal_one.classList.add("active");
 });
 
+button_aboutme.addEventListener("click", function(){
+    modal_two.classList.add("active");
+});
+
+
+
+
+
 // Modal close
 close_modal.addEventListener("click", function () {
   modal_one.classList.remove("active");
+ 
 });
 
 // Button click sound
